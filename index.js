cd555f3d6e323cba0a7635d66146294b72bfa03d
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-07 12:48:25 +0200

    refactor index.js

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ let http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+const PORT = 8080;
+const TEXT_HTML = { 'Content-Type': 'text/html' };
+
 http.createServer(function (req, res) {
   let q = url.parse(req.url, true);
   let fileName = q.pathname !== '/' ? '.' + q.pathname + '.html' : './index.html';
   fs.readFile(fileName, function (err, data) {
     if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.writeHead(404, TEXT_HTML);
       fs.readFile('./404.html', (err, data) => {
         res.write(data);
         return res.end();
       })
     } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, TEXT_HTML);
       res.write(data);
       return res.end();
     }
   });
-}).listen(8080);
+}).listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
