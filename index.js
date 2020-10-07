315c0849042ffbf11dd6dd68c3825719adcb4ba3
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-07 12:42:18 +0200

    implement index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 let http = require('http');
+var url = require('url');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+  let q = url.parse(req.url, true);
+  let fileName = q.pathname !== '/' ? '.' + q.pathname + '.html' : './index.html';
+  fs.readFile(fileName, function (err, data) {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      fs.readFile('./404.html', (err, data) => {
+        res.write(data);
+        return res.end();
+      })
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      return res.end();
+    }
   });
 }).listen(8080);
